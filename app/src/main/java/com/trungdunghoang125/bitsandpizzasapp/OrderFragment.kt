a4bc756dd1e31f4e505d8c1db3f6b80d7ce856a6
trungdunghoang <trungdunghoang125@gmail.com>
2022-06-18 17:52:54 +0700

    Implement scrollable toolbar

--- app/src/main/java/com/trungdunghoang125/bitsandpizzasapp/OrderFragment.kt
+++ app/src/main/java/com/trungdunghoang125/bitsandpizzasapp/OrderFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class OrderFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,10 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        val view =  inflater.inflate(R.layout.fragment_order, container, false)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        // Display appbar name on Material Toolbar
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        return view
     }
 }
