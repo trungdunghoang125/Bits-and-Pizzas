58b071212a7fa2f83c718ddaca2bd5cb1f6aa04e
trungdunghoang <trungdunghoang125@gmail.com>
2022-06-25 18:02:10 +0700

    Implementing view binding

--- app/src/main/java/com/trungdunghoang125/bitsandpizzasapp/OrderFragment.kt
+++ app/src/main/java/com/trungdunghoang125/bitsandpizzasapp/OrderFragment.kt
@@ -6,31 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.chip.Chip
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.trungdunghoang125.bitsandpizzasapp.databinding.FragmentOrderBinding
 
 class OrderFragment : Fragment() {
+    private var _binding:FragmentOrderBinding? = null;
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_order, container, false)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
-        toolbar.setTitleTextColor(Color.WHITE)
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        binding.toolbar.setTitleTextColor(Color.WHITE)
         // Display appbar name on Material Toolbar
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
 
         // FAB setOnClickListener
-        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
+
+        binding.fab.setOnClickListener {
             // code to execute when fab is click
-            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaGroup = binding.pizzaGroup
             val pizzaType = pizzaGroup.checkedRadioButtonId
             // No pizza was selected
             if (pizzaType == -1) {
@@ -43,13 +45,19 @@ class OrderFragment : Fragment() {
                     R.id.radio_diavolo -> "Diavolo pizza"
                     else -> "Funghi pizza"
                 }
-                val parmesan = view.findViewById<Chip>(R.id.parmesan)
-                val chilli = view.findViewById<Chip>(R.id.chilli_oil)
+                val parmesan = binding.parmesan
+                val chilli = binding.chilliOil
                 if (parmesan.isChecked) text += ", extra parmesan"
                 if (chilli.isChecked) text += ", extra chilli oil"
-                Snackbar.make(fab, text, Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.fab, text, Snackbar.LENGTH_LONG).show()
             }
         }
         return view
+    }
+
+    // set _binding = null after fragment no longer inflate layout
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
